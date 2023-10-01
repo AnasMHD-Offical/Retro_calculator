5894bd79469a4faead0e0670a18061a12506550d
Anas Muhammed <132032095+An-As006@users.noreply.github.com>
2023-10-01 16:55:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function btClick(val)
+{
+    document.getElementById("display").value+=val;
+}
+function allClear()
+{
+    document.getElementById("display").value="0";
+}
+function Clear()
+{
+    document.getElementById("display").value="0";
+}
+function equal()
+{
+    var text=document.getElementById("display").value;
+    var result=eval(text);
+    document.getElementById("display").value=result;
+}
